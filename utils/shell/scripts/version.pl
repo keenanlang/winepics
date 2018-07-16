d931bc5f58f49dfcb263cc4f5f2e5f8edf058e5e
Keenan Lang <klang@aps.anl.gov>
2018-07-16 17:37:10 -0500

    Update to use vswhere.exe when available

--- utils/shell/scripts/version.pl
+++ utils/shell/scripts/version.pl
@@ -1,14 +1,23 @@
-$found = 0;
-
-foreach (sort keys %ENV)
+if (! -e "C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vswhere.exe")
 {
-	if ($_ =~ /^VS([0-9]+)COMNTOOLS/)
+	$found = 0;
+
+	foreach (sort keys %ENV)
 	{
-		if ($1 > $found)
+		if ($_ =~ /^VS([0-9]+)COMNTOOLS/)
 		{
-			$found = $1;
+			if ($1 > $found)
+			{
+				$found = $1;
+			}
 		}
 	}
-}
 
-print $ENV{"VS" . $found . "COMNTOOLS"};
+	print $ENV{"VS" . $found . "COMNTOOLS"} . "\\..\\..\\VC\\";
+}
+else
+{
+	$found = `"C:\\Program Files (x86)\\Microsoft Visual Studio\\Installer\\vswhere.exe" -latest -property installationPath`
+	$found =~ s/[\r\n]+$//;
+	print $found . "\\VC\\Auxiliary\\Build\\";
+}
